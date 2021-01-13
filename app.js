caf66b1f7f6c3c4649a5a745400b0f076bd01206
Mahesh Madushan <madu140@gmail.com>
2021-01-13 18:05:50 +0530

    sign-in and log-in front -end

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ app.use(express.json());//send data with json format
 
 app.use(express.urlencoded({extended:false}));//not sending any dorm data
 
+app.set('view engine','ejs')
+
 app.use(cors()); // when we are having api call this will not block it and send data to backend
 
+app.use(express.static(__dirname+'/public'));//send data with json format
+
+app.get('/signin', (req,res) => {
+  res.render('signin-customer/index')
+})
+app.get('/login', (req,res) => {
+  res.render('login-customer/index')
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('app is running on port:' + process.env.PORT || 5000);
