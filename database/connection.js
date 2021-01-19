a92db97d0aeb33adf85004b3a703b54a2caa244d
Mahesh Madushan <madu140@gmail.com>
2021-01-19 17:13:07 +0530

    solved connecion issues

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,20 @@ var pool  = mysql.createPool({
   host            : process.env.HOST,
   user            : process.env.USER,
   password        : process.env.PASSWORD,
-  database        : process.env.DATABASE
+  database        : process.env.DATABASE,
+  waitForConnections:true,
+  queueLimit:900
 });
 
+pool.on('acquire', function (connection) {
+  console.log('Connection %d acquired', connection.threadId);
+});
+
+pool.on('connection', function (connection) {
+  console.log('connected')
+});
+
+pool.on('enqueue', function () {
+  console.log('Waiting for available connection slot');
+});
 module.exports = pool;
