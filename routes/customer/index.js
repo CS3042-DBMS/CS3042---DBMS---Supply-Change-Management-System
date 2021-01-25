b999d0623c328f3d3b2f47b3dddb91ce21d12193
vinoja98 <vinoiarathnayake@gmail.com>
2021-01-25 14:20:20 +0530

    validation implemented

--- routes/customer/index.js
+++ routes/customer/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const menu = require('./menu');
+const cart = require('./cart');
 
 router.use('/menu',menu);
+// router.use('/cart',cart);
 
 module.exports = router
