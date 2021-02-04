503292f44acfaf224107f2fc22c4ced276161807
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2021-02-04 22:06:20 +0530

    changes

--- controllers/manager/orders.js
+++ controllers/manager/orders.js
@@ -7,13 +7,8 @@ async function viewOrders(request,response){
     try {
         const res = await Manager.viewOrders();
         const result = JSON.parse(JSON.stringify(res[0]))
-<<<<<<< Updated upstream:controllers/manager/menu.js
-        //console.log(request);
-        response.render('manager/menu',{result: result});
-=======
         //console.log(result[0].order_id);
         response.render('manager/vieworders',{result: result});
->>>>>>> Stashed changes:controllers/manager/orders.js
         
     } catch (error) {
         response.send(error.message);
