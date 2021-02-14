7628164747a7aa3fa38bdb53731af8f86de98cef
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2021-02-14 12:40:30 +0530

    quarterly sales report done

--- controllers/manager/quarterly_sales_report.js
+++ controllers/manager/quarterly_sales_report.js
@@ -6,7 +6,7 @@ const Manager = require('../../models/manager/manager');
 async function viewQuarterlySalesReport(request,response){
 
     try {
-        const result = await Manager.viewQuarterlySalesReport();
+        const result = await Manager.viewQuarterlySalesReport(request);
         const jsresult = JSON.parse(JSON.stringify(result[0]));
         response.render('manager/quarterly_sales_report',{report: jsresult});
     }
