6a35c18ff3fdd58702383d8194a00558ad297a49
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2021-02-16 19:38:59 +0530

    Update index.js
    bug fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {requireAuthCustomer,requireAuthManager,requireAuthStoreManager,requireAuthDriver,requireAuthAssistant} = require('../middleware/authMiddleware') // add this middle ware to authenticate without login
 
-
 // protect customer routes - add requireAuthCustomer middleware
 // protect manager routes - add requireAuthManager middleware
 // protect assistant routes - add requireAuthAssistant middleware
@@ -11,20 +10,37 @@ const {requireAuthCustomer,requireAuthManager,requireAuthStoreManager,requireAut
 
 
 const customerFunctions = require('./customer');
-const driverFunctions = require('./driver');
 const storeManagerFunctions = require('./storemanager');
 const ManagerFunctions = require('./manager');
-const assistantdriverFunctions = require('./assistantdriver');
+const authRoutes = require('./authroutes/authroute')
+const managerFunctions = require('./manager');
 
 // manager routes
 router.use('/manager_func',requireAuthManager,managerFunctions);
+
+
+
 // customer routes
 router.use('/customer_func', requireAuthCustomer,customerFunctions);
+
+
+
+
 //store manager routes
 router.use('/storemanager',requireAuthStoreManager,storeManagerFunctions); 
+
+
+
+
 //manager routes
 router.use('/manager_func',requireAuthManager,ManagerFunctions);
+
+
+
+
+
 // autentication routes
 router.use('/',authRoutes)
+
 
 module.exports = router
