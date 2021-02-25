bc5d428c533063405df2622dd8aceb6584175e5d
vinoja98 <vinoiarathnayake@gmail.com>
2021-02-25 11:51:55 +0530

    DONE

--- database/remotemysql.sql
+++ database/remotemysql.sql
@@ -292,3 +292,39 @@ CREATE or replace DEFINER=`root`@`localhost` EVENT `e_weekly` ON SCHEDULE EVERY 
         UPDATE `driver_rosters` SET `worked_hours`= 0 WHERE 1;
       END
 $$
+
+-- trigger
+DELIMITER
+$$
+CREATE TRIGGER `a_r_update_trigger` AFTER INSERT ON `driver_assistant`
+FOR EACH ROW BEGIN
+IF NOT EXISTS (SELECT assistant_id FROM assistant_rosters WHERE assistant_id = NEW.assistant_id) THEN
+INSERT INTO `cs3042-dbms`.`assistant_rosters`
+(`assistant_id`,
+`schedule_id`,
+`worked_hours`,
+`consecutive_schedules`,
+`working_hours`)
+VALUES
+( NEW.assistant_id ,
+NULL,
+0,
+0,
+0);
+END IF;
+END
+$$
+
+DELIMITER
+$$
+CREATE TRIGGER `x_r_update_trigger` AFTER INSERT ON `driver`
+FOR EACH ROW BEGIN
+IF NOT EXISTS (SELECT driver_id FROM driver_rosters WHERE driver_id = NEW.driver_id) THEN
+INSERT INTO `cs3042-dbms`.`driver_rosters`
+(`driver_id`,
+`schedule_id`,
+`worked_hours`,
+`working_hours`)
+VALUES (NEW.driver_id,null,0,0); END IF;
+END
+$$
